f27c2b504b75b9a7dcb3a6be3991911ea2885c5f
Vikas8920 <vikasmishra0306@gmail.com>
2025-04-12 10:56:09 +0530

    12-04-2025

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from './Components/Form'
 import MultipleFieldForm from './Components/MultipleFieldForm'
 import TextArea from './Components/TextArea'
 import Select from './Components/Select'
 import RadioButton from './Components/RadioButton'
+import Checkbox from './Components/Checkbox'
+import DisplayForm from './Components/DisplayForm'
+import DisplayData from './Components/DisplayData'
+import AnotherForm from './Components/AnotherForm'
 
 const App = () => {
+  const [formData, setFormData] = useState(null)
+
+  const handleFormSubmit = (data) =>{
+    setFormData(data)
+  }
   return (
     <>
       <Form/>
@@ -17,6 +26,16 @@ const App = () => {
       <Select/>
       <hr/>
       <RadioButton/>
+      <hr/>
+      <Checkbox/>
+      <hr/>
+      <DisplayForm onFormSubmit={handleFormSubmit}/>
+      {formData && <DisplayData data={formData}/>}
+
+      {/* if formData exists, render the displayData component */}
+
+      <hr/>
+      <AnotherForm/>
     </>
   )
 }
---
+++ src/Components/DisplayForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const DisplayForm = ({onFormSubmit}) => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        onFormSubmit({name, email, phone})
+        setName('')
+        setEmail('')
+        setPhone('')
+    }
+  return (
+    <>
+      <form action='' method='post' onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor='name'>Name:</label>
+            <input type='text' value={name} onChange={(e)=>setName(e.target.value)} required id='name'/>
+        </div>
+        <div>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} required id='email'/>
+        </div>
+        <div>
+            <label htmlFor='phone'>Phone:</label>
+            <input type='tel' value={phone} onChange={(e)=>setPhone(e.target.value)} required id='phone'/>
+        </div>
+        <div>
+           <button type='submit'>Submit</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default DisplayForm
