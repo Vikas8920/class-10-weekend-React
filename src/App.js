721d3d86a575f903435dc06eea41a58a5867110b
Vikas8920 <vikasmishra0306@gmail.com>
2025-05-03 10:59:02 +0530

    03-05-2025

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './Components/Home'
-import About from './Components/About'
-import Blog from './Components/Blog'
-import Contact from './Components/Contact'
-import './App.css'
-import NoPage from './Components/NoPage'
-import ParentComponent from './Components/ParentComponent'
+import FavoriteColor from './Components/FavoriteColor'
+import Car from './Components/Car'
+import Timer from './Components/Timer'
+import Counter from './Components/Counter'
 
 const App = () => {
   return (
     <>
-     <Router>
-       <Navbar/>
-       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/blogs' element={<Blog/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='*' element={<NoPage/>}/>
-       </Routes>
-     </Router>
-
-     <hr/>
-     <ParentComponent/>
+      <FavoriteColor/>
+      <hr/>
+      <Car/>
+      <hr/>
+      <Timer/>
+      <hr/>
+      <Counter/>
     </>
   )
 }
